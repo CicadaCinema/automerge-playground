b794b3f148907b59d122b39e844caab5399374b4
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-05-09 19:24:24 +0100

    install MUI, add static components in roughly the correct places

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import './App.css';
+
+function MyField({fieldLabel} : {fieldLabel: string}) {
+  return (
+    <TextField
+      fullWidth
+      id="outlined-multiline-static"
+      label={fieldLabel}
+      multiline
+      rows={4}
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={2} columns={2}>
+        <Grid item xs={2}>
+          <MyField fieldLabel={"Initial text"}/>
+        </Grid>
+        <Grid item xs={1}>
+          <MyField fieldLabel={"User 1"}/>
+        </Grid>
+        <Grid item xs={1}>
+          <MyField fieldLabel={"User 2"}/>
+        </Grid>
+        <Grid item xs={1}>
+          <h2>User 1 result</h2>
+          <p>Something</p>
+        </Grid>
+        <Grid item xs={1}>
+          <h2>User 2 result</h2>
+          <p>Something else</p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
